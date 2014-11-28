4371e6342b86d4ec121fb102695b0c6e42ebd0f6
galanarus <raimond.kowski@student.fh-kiel.de>
2014-11-28 09:01:44 +0100

    form POST to .php; index5.html modified

---
+++ result.php
@@ -0,0 +1,10 @@
+<?php
+
+    echo "<h1>Formularauswertung </h1>";
+
+    echo "<p>Login: " . $_POST[login] . "</p>";
+    echo "<p>Password: " . $_POST[pwd] . "</p>";
+    echo "<p>Gender: " . $_POST[gender] . "</p>";
+    echo "<p>EMail: " . $_POST[email] . "</p>";
+    echo "<p>Datum: " . $_POST[datum] . "</p>";
+?>
